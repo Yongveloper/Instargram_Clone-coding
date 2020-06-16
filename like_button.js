b17748baaafd6cb4c2d6cff9d89c8374e22d8b66
YongCoding <aaa9015@naver.com>
2020-06-16 19:46:06 +0900

    JavaScript: Like Button Action

--- like_button.js
+++ like_button.js
@@ -2,12 +2,57 @@
 
 
 const likeNullBtn = document.querySelector('.like-null-btn');
+const likeNullBtn2 = document.querySelector('.like-null-btn2');
+const likeNullBtn3 = document.querySelector('.like-null-btn3');
+
 const likeActionBtn = document.querySelector('.like-action-btn');
+const likeActionBtn2 = document.querySelector('.like-action-btn2');
+const likeActionBtn3 = document.querySelector('.like-action-btn3');
+
 const CLICKED_CLASS = 'hide';
+
 const likeCount= document.querySelector('#like-count');
+const likeCount2= document.querySelector('#like-count2');
+const likeCount3= document.querySelector('#like-count3');
 
 
+function nullFunction(elm) {     
+    const nullBtn = document.querySelector('.'+elm);          
+    nullBtn.classList.add(CLICKED_CLASS);
+    if(elm==='like-null-btn'){
+    likeActionBtn.classList.remove(CLICKED_CLASS);
+    likeCount.innerHTML = "좋아요 1,295개";
+    } else if (elm==='like-null-btn2') {
+        likeActionBtn2.classList.remove(CLICKED_CLASS);
+        likeCount2.innerHTML = "좋아요 3,350개";
+    } else {
+        likeActionBtn3.classList.remove(CLICKED_CLASS);
+        likeCount3.innerHTML = "좋아요 3,000개";
+    }
+};
 
+function actionFunction(elm) { 
+    const actionBtn = document.querySelector('.'+elm);    
+    actionBtn.classList.add(CLICKED_CLASS);
+    if(elm==='like-action-btn'){
+    likeNullBtn.classList.remove(CLICKED_CLASS);
+    likeCount.innerHTML = "좋아요 1,294개";
+    } else if (elm==='like-action-btn2'){
+        likeNullBtn2.classList.remove(CLICKED_CLASS);
+        likeCount2.innerHTML = "좋아요 3,349개";
+    } else {
+        likeNullBtn3.classList.remove(CLICKED_CLASS);
+        likeCount3.innerHTML = "좋아요 2,999개";
+    }
+};
+
+
+function init() {
+    
+}
+init();
+
+/*
 likeNullBtn.addEventListener('click', ()=> {
     likeNullBtn.classList.add(CLICKED_CLASS);
     likeActionBtn.classList.remove(CLICKED_CLASS);
@@ -56,6 +101,4 @@ likeActionBtn3.addEventListener('click', ()=>{
     likeNullBtn3.classList.remove(CLICKED_CLASS);
     likeCount3.innerHTML = "좋아요 2,999개";
 });
-
-
-
+*/
