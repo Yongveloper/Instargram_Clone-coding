8f37a6678ac906c93ba11c8e5ec0ee31c134a0cf
YongCoding <aaa9015@naver.com>
2020-06-18 04:15:17 +0900

    JS: Like Button

--- like_button.js
+++ like_button.js
@@ -1,54 +1,29 @@
 'use strict';
-// Put on hold
 
+const likeBtn   = document.querySelectorAll('.heart');
 
-const likeNullBtn = document.querySelector('.like-null-btn');
-const likeNullBtn2 = document.querySelector('.like-null-btn2');
-const likeNullBtn3 = document.querySelector('.like-null-btn3');
+[].forEach.call(likeBtn, function(likeBtn){
+    likeBtn.addEventListener("click",btnClick,false) ;
+}) ;
 
-const likeActionBtn = document.querySelector('.like-action-btn');
-const likeActionBtn2 = document.querySelector('.like-action-btn2');
-const likeActionBtn3 = document.querySelector('.like-action-btn3');
+function btnClick(e){    
+    
+    const likeIcon = e.path[0]        
 
-const CLICKED_CLASS = 'hide';
+    const likeCount=likeIcon.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].childNodes[1];        
 
-const likeCount= document.querySelector('#like-count');
-const likeCount2= document.querySelector('#like-count2');
-const likeCount3= document.querySelector('#like-count3');
+    let likeCountInt;
 
+    if(likeIcon.classList.contains('fa-heart-o')){
+    likeCountInt= parseInt(likeCount.innerHTML)+1;
+    
+    } else{
+    likeCountInt= parseInt(likeCount.innerHTML)-1;
+    
+    };
 
-function nullFunction(elm) {     
-    const nullBtn = document.querySelector('.'+elm);          
-    nullBtn.classList.add(CLICKED_CLASS);
-
-    if(elm === 'like-null-btn'){
-        likeActionBtn.classList.remove(CLICKED_CLASS);
-        likeCount.innerHTML = "5";
-
-    } else if (elm === 'like-null-btn2') {
-        likeActionBtn2.classList.remove(CLICKED_CLASS);
-        likeCount2.innerHTML = "50";
-
-    } else {
-        likeActionBtn3.classList.remove(CLICKED_CLASS);
-        likeCount3.innerHTML = "3,000";
-    }
-};
-
-function actionFunction(elm) { 
-    const actionBtn = document.querySelector('.'+elm);    
-    actionBtn.classList.add(CLICKED_CLASS);
-
-    if(elm === 'like-action-btn'){
-        likeNullBtn.classList.remove(CLICKED_CLASS);
-        likeCount.innerHTML = "4";
-
-    } else if (elm === 'like-action-btn2'){
-        likeNullBtn2.classList.remove(CLICKED_CLASS);
-        likeCount2.innerHTML = "49";
-        
-    } else {
-        likeNullBtn3.classList.remove(CLICKED_CLASS);
-        likeCount3.innerHTML = "2,999";
-    }
-};
+    likeCount.innerHTML=likeCountInt;
+     
+    likeIcon.classList.toggle('fa-heart-o');
+    likeIcon.classList.toggle('fa-heart');      
+}
